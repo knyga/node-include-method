6fab508f498a8403d191305ed720c8f93893eb71
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-06-19 18:20:05 +0200

    compiles from directory with multiple src

--- test/include.js
+++ test/include.js
@@ -3,7 +3,7 @@
 var Include = require('../lib/include');
 var assert = require('assert');
 var fs = require('fs');
-var glob = require('glob');
+var glob = require("multi-glob").glob;
 var rimraf = require('rimraf');
 var path = require('path');
 
@@ -183,6 +183,27 @@ describe('include', function () {
 
                     assert.equal(ctn['t5.js'], 'var html = \'<ul><li class="some name">One</li><li class="">Two</li><li>Three</li><li class=something>Four</li><li class="x y z">Fix</li></ul>\';');
                     assert.equal(ctn['t6.js'], 'var html = \'<div id=main-body ui-view=app><div id=main-loading><span class=fa-browser-ok><span class=loader fa-display-fade=AppState.isLoading()></span><ul class="loader-anim lt-ie10"><li></li><li></li><li></li><li></li><li></li></ul></span><h1>FollowAnalytics</h1><h2><span class=fa-browser-error>Unfortunately, FollowAnalytics is not compatible with Internet Explorer 9 and lower. Please upgrade your browser or use another one.</span> <span class=fa-browser-ok ng-cloak>{{AppState.getText()}}</span></h2></div></div><script src=/bower_components/modernizr/modernizr.js></script><script>alert(\\\'123\\\');</script>\';');
+                    done();
+                });
+            }
+        });
+    });
+
+    it('compiles from directory with multiple src', function(done) {
+        rimraf.sync('./test/testdata/output');
+        includeObj.compile({
+            src: ['./test/testdata/includes/t1.js', './test/testdata/includes/t2.js'],
+            dest: './test/testdata/output',
+            done: function() {
+                glob('./test/testdata/output/@(t1|t2).js', function(err, files) {
+                    var ctn = {};
+                    files.forEach(function(file) {
+                        ctn[path.basename(file)] = fs.readFileSync(file).toString();
+                    });
+
+                    assert.equal(ctn['t1.js'], "var data = 5;");
+                    assert.equal(ctn['t2.js'], "var data1 = 5;\nvar data2 = 6;");
+
                     done();
                 });
             }
